5bf56125da1e0dcaebeee8a51746899fe3e2235b
Scott Grunerud <sgrunerud@edu.uwaterloo.ca>
2020-07-24 18:23:26 +0000

    done

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
-}
+const sumItems = function (array) {
+  let sum = 0;
+  if (array.length === 1) {
+    return array[0];
+  }
+
+  array.forEach((item) => {
+    if (Array.isArray(item)) {
+      sum += parseInt(sumItems(item));
+    } else {
+      sum += item;
+    }
+  });
+  console.log(sum);
+
+  return sum;
+};
 
 module.exports = sumItems;
